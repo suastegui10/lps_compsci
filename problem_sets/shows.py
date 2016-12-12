de388ee8cc86c559c13031bc52348805a7f61015
Asael Suastegui <rc.suastegui.asael@leadps.org>
2016-12-12 04:22:58 +0000

    shows code

--- problem_sets/shows.py
+++ problem_sets/shows.py
@@ -1,3 +1,22 @@
 print("Welcome to PumaPix!")
 
+print("Enter your 5 favorite TV shows")
+shows = 0
+favs = []
+while shows < 5:
+	print("Enter your show name:")
+	tv = raw_input()
+	favs.append(tv)
+	shows = shows + 1
+
+print("Ok, here's what you entered:" + str(favs))
+print("We added some shows that are better")
+
+bettershows = ["Spongebob" , "Fuller House"]
+together = favs + bettershows
+together.sort()
+y = 0
+for x in together:
+	y = y + 1
+	print(str(y) + "." + x)
 
